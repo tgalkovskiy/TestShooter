90f7a002d76593589f59580fa252d253de94be09
timurg <timurg@playtika.com>
2020-10-14 18:48:43 +0300

    Camera(2players) and render texture

---
+++ TestGalkovskiy(Dmitry-Minsky)/Assets/MainCamera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform Body;
+    [SerializeField] 
+    private Transform Player;
+    [SerializeField] 
+    private float mouseSensitivity = 100f;
+    private float Xrot;
+    
+    private void Update()
+    {
+        var MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        Player.Rotate(0, 1 * MouseX, 0);
+        var MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        Body.Rotate(MouseY,0,0);
+        //Xrot = -MouseY;
+        //Xrot = Mathf.Clamp(Xrot, -60f, 60f);
+        //Head.localRotation = Quaternion.Euler(Xrot, 0, 0);
+        //.Rotate(Vector3.up * MouseX);
+
+    }
+}
+
---
+++ TestGalkovskiy(Dmitry-Minsky)/Assets/MovePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private float Speed, Sens;
+    private Vector3 Velocity;
+    public GameObject Player2;
+    [SerializeField]
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Start()
+    {
+        
+    }
+    private void MovmentPlayerOne()
+    {
+        Velocity = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Velocity*Speed*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.I)||Input.GetKey(KeyCode.K)||Input.GetKey(KeyCode.J)||Input.GetKey(KeyCode.L))
+        {
+            Player2.transform.Translate(Velocity*Speed*Time.deltaTime);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MovmentPlayerOne();
+    }
+}
