95a18e0a5efa79bd8d269b2e308b09e54ccbc254
timurg <timurg@playtika.com>
2020-10-19 14:39:40 +0300

    relload

---
+++ TestGalkovskiy(Dmitry-Minsky) 14.36.58/Assets/Editor/EditorColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DefoltColor))]
+public class EditorColor : Editor
+{
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        DefoltColor defolt = target as DefoltColor;
+        if (GUILayout.Button("Defolt Color"))
+        {
+            defolt.Defolt();
+            //defolt.GetComponent<Renderer>().sharedMaterial.color = Color.cyan;
+            //defolt.GetComponent<MeshRenderer>().material.color = defolt.Defolt();
+        }
+        
+    }
+}
---
+++ TestGalkovskiy(Dmitry-Minsky) 14.36.58/Assets/MainCamera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform Body;
+    [SerializeField] 
+    private Transform Player;
+    [SerializeField] 
+    private Camera CameraPlayer;
+    [SerializeField]
+    private float mouseSensitivity;
+    [SerializeField]
+    private float YAxisAngleLock = 90f;
+    private Vector2 Rotation;
+    private Quaternion PlayerTargetRot;
+    private Quaternion CameraTargetRot;
+    
+    private void Start()
+    {
+        Player = transform;
+        PlayerTargetRot = Player.rotation;
+        CameraTargetRot = CameraPlayer.transform.rotation;
+    }
+
+    private void RotCam()
+    {
+        var MouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        Player.Rotate(0, 1 * MouseX, 0);
+        var MouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        Body.Rotate(MouseY,0,0);
+    }
+
+    private void ZoomCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            
+            CameraPlayer.fieldOfView = Mathf.Lerp(CameraPlayer.fieldOfView,30, Time.deltaTime*10);
+        }
+        else
+        {
+            CameraPlayer.fieldOfView = Mathf.Lerp(CameraPlayer.fieldOfView, 60, Time.deltaTime*10);
+        }
+        
+    }
+    private void Update()
+    {
+        RotCam();
+        ZoomCamera();
+    }
+}
+
