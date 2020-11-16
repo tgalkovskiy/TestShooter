18932af2e40fe1d4639fecc6b83c30ecd55fa7d5
timurg <timurg@playtika.com>
2020-11-16 14:20:06 +0300

    try/catch

---
+++ TestGalkovskiy(Dmitry-Minsky) 14.36.58/Assets/Scripts/DefoltColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefoltColor : MonoBehaviour
+{
+    
+    private Color DefualtColor;
+    private void Start()
+    {
+        DefualtColor = this.GetComponent<MeshRenderer>().material.color;
+
+    }
+
+    public void Defolt()
+    {
+        this.GetComponent<MeshRenderer>().material.color = DefualtColor;
+    }
+    
+}
---
+++ TestGalkovskiy(Dmitry-Minsky) 14.36.58/Assets/encoding/Encoding.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using System.IO.Compression;
+using System.IO;
+using UnityEngine.Networking;
+
+public class Settings
+{
+   public float Speed;
+   public int Health;
+   public string Name;
+   public string Base64;
+}
+
+public class Encoding : MonoBehaviour
+{
+   private Settings PlyerSetting;
+   private static string PathDowland = "https://dminsky.com/settings.zip";
+   private static string PathSaveZip = Path.Combine("Assets/Unzip!", "settings.zip");
+   private static string PathSaveFile = Path.Combine("Assets/Unzip!");
+   private static string PathReadFile = Path.Combine("Assets/Unzip!", "settings.json");
+   [MenuItem("Tools/DowlandSetting")]
+   public static void DowlandSetting()
+   {
+      try
+      {
+         UnityWebRequest UWR = UnityWebRequest.Get(PathDowland);
+         UWR.downloadHandler = new DownloadHandlerFile(PathSaveZip);
+         var aspp = UWR.SendWebRequest();
+      }
+      catch (Exception e)
+      {
+         Debug.Log("Error NetWork");
+      }
+      finally
+      {
+         Debug.Log("Completed Dowland");
+         AssetDatabase.Refresh();
+      }
+   }
+
+   [MenuItem("Tools/UnZip")]
+   public static void UnZip()
+   {
+      try
+      {
+         ZipFile.ExtractToDirectory(PathSaveZip, PathSaveFile);
+         AssetDatabase.Refresh();
+      }
+      catch (Exception e)
+      {
+         Debug.Log("Error Extract");
+      }
+      finally
+      {
+         Debug.Log("Completed Extract");
+         AssetDatabase.Refresh();
+      }
+         
+   }
+
+}
+
